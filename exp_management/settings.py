e7be94ef0fc2c24bf32bc30f727cb006a4cf6975
Yash-124 <113996104+Yash-124@users.noreply.github.com>
2024-11-22 16:48:01 +0530

    Update settings.py

--- exp_management/settings.py
+++ exp_management/settings.py
@@ -18,7 +18,7 @@ STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
